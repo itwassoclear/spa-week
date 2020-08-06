6d41a33922a9503ae0a22be741aac8846163dffa
Maria Kotliarova <itwassoclear@gmail.com>
2020-08-06 15:28:24 +0300

    deploy

---
+++ precache-manifest.ba4235daa5e0b1402892f51414375725.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bb53bd174bf8a8c445",
+    "url": "/spa-week/css/app.925c9e40.css"
+  },
+  {
+    "revision": "9321fa0276a00d5f2257",
+    "url": "/spa-week/css/chunk-vendors.3e4956bb.css"
+  },
+  {
+    "revision": "0dd3e602c9353c39d32eef64e05ba2a8",
+    "url": "/spa-week/index.html"
+  },
+  {
+    "revision": "18bb53bd174bf8a8c445",
+    "url": "/spa-week/js/app.9571e3ac.js"
+  },
+  {
+    "revision": "9321fa0276a00d5f2257",
+    "url": "/spa-week/js/chunk-vendors.add843d1.js"
+  },
+  {
+    "revision": "d9809b7d661e1a5db715792552ee01e0",
+    "url": "/spa-week/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/spa-week/robots.txt"
+  }
+]);
